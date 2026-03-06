5effe4b1d6a06a87717f8107ac1f61ab39954db4
Erkan HAZIR <41erkan2002@gmail.com>
2026-03-06 16:29:13 +0300

    feat: update scripts and configurations for improved development workflow and add new ignore patterns

--- apps/gateway-service/src/common/constants/route-map.constant.ts
+++ apps/gateway-service/src/common/constants/route-map.constant.ts
@@ -1,4 +1,4 @@
-export const ROUTE_MAP = {
+export const ROUTE_MAP: { [key: string]: string } = {
   '/api/auth': 'http://auth-service:3000',
   '/api/products': 'http://product-service:3000',
   '/api/orders': 'http://order-service:3000',
