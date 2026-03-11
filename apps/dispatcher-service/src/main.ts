fe3d282e3a985b5301b2922b881c1ee386810585
Erkan HAZIR <41erkan2002@gmail.com>
2026-03-11 15:02:05 +0300

    Dispatcher add packages

---
+++ apps/dispatcher-service/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { add } from '@e-ticaret/database';
+
+(() => {
+  const a: number = add(1, 2) as number;
+  console.log(a);
+})()
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
