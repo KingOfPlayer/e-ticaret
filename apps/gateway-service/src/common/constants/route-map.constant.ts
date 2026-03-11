a16710b621c2eebac3e801e4474948a8c518a3ee
Erkan HAZIR <41erkan2002@gmail.com>
2026-03-11 10:35:19 +0300

    Gateway revert changes

--- apps/gateway-service/src/common/constants/route-map.constant.ts
+++
@@ -1,7 +0,0 @@
-export const ROUTE_MAP: { [key: string]: string } = {
-  '/api/auth': 'http://auth-service:3000',
-  '/api/products': 'http://product-service:3000',
-  '/api/orders': 'http://order-service:3000',
-  '/api/admin/orders': 'http://order-service:3000',
-  '/api/admin/logs': 'http://localhost:3000',
-};
