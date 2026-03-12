eb5e134d815019bf4876b88dbc494ca31ae724ee
Erkan HAZIR <41erkan2002@gmail.com>
2026-03-13 02:45:28 +0300

    fix: update service ports and URLs in docker-compose.yml and main.ts files

--- apps/dispatcher-service/src/main.ts
+++ apps/dispatcher-service/src/main.ts
@@ -9,6 +9,6 @@ import { add } from '@e-ticaret/database';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
