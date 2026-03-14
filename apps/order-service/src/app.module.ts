48f9e63299163661dab25869eb6878b59b5a10a4
FerhatSezgin <153099536+FerhatSezgin@users.noreply.github.com>
2026-03-14 17:30:23 +0300

    feat(order-service): implement orders service, module and database setup

--- apps/order-service/src/app.module.ts
+++ apps/order-service/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/orders',
+    ),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
