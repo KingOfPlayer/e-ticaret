0eda92d9b4644123535bc2793657f5bdea72bfed
Erkan HAZIR <41erkan2002@gmail.com>
2026-03-31 02:06:29 +0300

    feat: update RouteResolverController to include logging and error handling, and modify RouteTable request for proper URL and authentication

--- apps/gateway-service/src/modules/resolver/resolver.controller.ts
+++ apps/gateway-service/src/modules/resolver/resolver.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, All, Req, Res, Param, NotFoundException, UseGuards, Get, Post, Patch } from '@nestjs/common';
+import { Controller, All, Req, Res, Param, NotFoundException, UseGuards, Get, Post, Patch, BadRequestException } from '@nestjs/common';
 import * as express from 'express';
 import { RouteResolverService } from './route.resolver.service';
 import { LoggerService } from '@e-ticaret/logger';
-import { RoleGuard } from '@e-ticaret/role';
+import { RoleGuard, Roles, UserRole } from '@e-ticaret/role';
 
 @Controller("route")
 @UseGuards(RoleGuard)
+@Roles(UserRole.Admin)
 export class RouteResolverController {
   constructor(
     private readonly routeResolverService: RouteResolverService,
+    private readonly logger: LoggerService,
   ) {}
+
+  @Get()
+  async getRoute(@Req() req: express.Request, @Res() res: express.Response) {
+    const routes = await this.routeResolverService.getAllRoutes();
+    return res.status(200).json(routes);
+  }
 
   @Patch()
   async getRoutes(@Req() req: express.Request, @Res() res: express.Response) {
     const routes = await this.routeResolverService.refleshRoutes();
+    this.logger.info(`Routes refreshed successfully`, 'RouteResolverController');
     return res.status(200).json(routes);
   }
 
@@ -21,9 +30,10 @@ export class RouteResolverController {
   async addRoute(@Req() req: express.Request, @Res() res: express.Response) {
     const { prefix, target } = req.body;
     if (!prefix || !target) {
-      return res.status(400).json({ message: 'Prefix and target are required' });
+      return new BadRequestException('Prefix and target are required');
     }
     await this.routeResolverService.addRoute({ prefix, target });
+    this.logger.info(`Added new route: ${prefix} -> ${target}`, 'RouteResolverController');
     return res.status(201).json({ message: 'Route added successfully' });
   }
 }
